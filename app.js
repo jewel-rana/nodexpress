7cca043ba90bbfb33b326351ed79f9e105000fd8
Jewel Rana <jewelrana.dev@gmail.com>
2019-04-17 21:53:53 +0600

    Update Course & Delete Course completed

--- app.js
+++ app.js
@@ -23,13 +23,11 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-    const schema = {
-        name: Joi.string().min(6).required()
-    };
-    const result = Joi.validate(req.body, schema);
-    // console.log(result);
-    if (result.error)
-        return res.status(400).send(result.error.details[0].message);
+    
+    const error = validate(req.body);
+
+    if (error)
+        return res.status(400).send(error);
     
     const course = {
         id: courses.length + 1,
@@ -37,8 +35,48 @@ app.post('/api/courses', (req, res) => {
     };
 
     courses.push(course);
-    res.send(course);
+    res.send(courses);
 });
+
+app.put('/api/courses/:id', (req, res) => {
+    const error = validate(req.body);
+
+    if (error)
+        return res.status(400).send(error);
+    
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+
+    if (! course)
+        return res.status(404).send('The course you are updating not found.');
+    
+    course.name = req.body.name;
+
+    return res.send( courses );
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+
+    if (!course)
+        return res.status(404).send("The course you want to delete does not found.");
+    
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    return res.send(courses);
+});
+
+function validate(data) {
+    const schema = {
+      name: Joi.string()
+        .min(6)
+        .required()
+    };
+    const result = Joi.validate(data, schema);
+    // console.log(result);
+    if (result.error)
+        return result.error.details[0].message;
+}
 
 //PORT
 const port = process.env.PORT || 3000;
